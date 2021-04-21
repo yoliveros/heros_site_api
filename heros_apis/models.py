fc064c9366c3adc15c422842decde16b0c27ab45
Yoelvis Oliveros <yoelvisoliveros@gmail.com>
2021-04-21 09:48:29 -0400

    precommit files

--- heros_apis/models.py
+++ heros_apis/models.py
@@ -5,7 +5,8 @@ import uuid
 
 
 class User(models.Model):
-    id = models.CharField(primary_key=True, max_length=64, default=uuid.uuid1())
+    id = models.CharField(primary_key=True, max_length=64, 
+    default=uuid.uuid1())
     email = models.EmailField()
     user_name = models.CharField(max_length=64)
     password = models.CharField(max_length=256)
